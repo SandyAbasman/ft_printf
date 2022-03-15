ade11936a63b9f90a0875578699274aad0e728de
Sunday Abasman <74963378+SandyAbasman@users.noreply.github.com>
2022-03-15 22:08:26 +0400

    Add files via upload

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -3,9 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asandy <asandy@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*   By: asandy <abasifrekesandy@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/09 22:06:29 by asandy            #+#    #+#             */
-/*   Updated: 2022/03/09 22:06:31 by asandy           ###   ########.fr       */
+/*   Created: 2022/03/15 21:52:23 by Asandy            #+#    #+#             */
+/*   Updated: 2022/03/15 21:52:37 by asandy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+ 
+
+
+int_printf(const char *s, ...)
+{
+    int len;
+    int i;
+    va_list args;
+
+    len = 0;
+    i = 0;
+    va_start(args, s);
+    while (s[i])
+    {
+        if (s[i] != '%')
+            len += write (1, &s[i], 1);
+        if (s[i] == '%')
+        {
+            len += ft_checker(s[i + 1], &args);
+            i++;
+        }
+        i++;
+    }
+    va_end(args);
+    return (len);
+}
